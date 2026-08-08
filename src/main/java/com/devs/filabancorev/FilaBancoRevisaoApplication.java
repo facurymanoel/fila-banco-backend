2be306c975a7087eb06cc10dca70ff6e227825c5
Dalmo Facuri <mffacuri@terra.com.br>
2026-08-08 13:54:46 -0300

    primeiro commit

---
+++ src/main/java/com/devs/filabancorev/FilaBancoRevisaoApplication.java
@@ -0,0 +1,13 @@
+package com.devs.filabancorev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilaBancoRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilaBancoRevisaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devs/filabancorev/FilaBancoRevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.filabancorev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaBancoRevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
