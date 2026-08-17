9e012fa20ab8c391260e76fc25f2606098ca3010
Dalmo Facuri <mffacuri@terra.com.br>
2026-08-17 14:29:28 -0300

    Criado implementação de testes unitários para proximaSenha()

--- src/test/java/com/devs/filabancorev/service/SenhaServiceTest.java
+++ src/test/java/com/devs/filabancorev/service/SenhaServiceTest.java
@@ -2,6 +2,7 @@ package com.devs.filabancorev.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -15,6 +16,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.devs.filabancorev.dto.ProximaSenhaDTO;
 import com.devs.filabancorev.dto.SenhaDTO;
 import com.devs.filabancorev.enums.StatusSenha;
 import com.devs.filabancorev.enums.TipoSenha;
@@ -188,6 +190,244 @@ public class SenhaServiceTest {
 		assertEquals("N001", resultado.getCodigo());
 		assertNotNull(resultado.getDataCriacao());
 
+	}
+	
+	@Test
+	void deveLancarExcecaoQuandoJaExisteSenhaEmAtendimento() {
+
+		Senha senhaAtendendo = new Senha();
+		senhaAtendendo.setStatus(StatusSenha.ATENDENDO);
+
+		when(senhaRepository.buscarSenhaEmAtendimento())
+		     .thenReturn(Optional.of(senhaAtendendo));
+
+		RuntimeException excecao = assertThrows(
+				RuntimeException.class, 
+				() -> senhaService.proximaSenha());
+
+		assertEquals(
+		    "Finalize o atendimento atual antes de chamar uma nova senha.",
+		    excecao.getMessage()
+        );
+	}
+	
+	@Test
+	void deveChamarSenhaPreferencialQuandoNaoExisteAtendimento() {
+
+		when(senhaRepository.buscarSenhaEmAtendimento())
+		    .thenReturn(Optional.empty());
+
+		Senha senhaPreferencial = new Senha();
+		senhaPreferencial.setCodigo("P001");
+		senhaPreferencial.setTipo(TipoSenha.PREFERENCIAL);
+		senhaPreferencial.setStatus(StatusSenha.AGUARDANDO);
+
+		when(senhaRepository.buscarProximaPreferencial())
+		     .thenReturn(Optional.of(senhaPreferencial));
+
+		ProximaSenhaDTO resultado = senhaService.proximaSenha();
+
+		assertEquals("P001", resultado.getCodigo());
+		assertEquals(StatusSenha.ATENDENDO, senhaPreferencial.getStatus());
+
+	}
+	
+	@Test
+	void deveChamarSenhaNormalQuandoNaoExistePreferencial() {
+
+		when(senhaRepository.buscarSenhaEmAtendimento())
+		    .thenReturn(Optional.empty());
+
+		when(senhaRepository.buscarProximaPreferencial())
+		    .thenReturn(Optional.empty());
+
+		Senha senhaNormal = new Senha();
+		senhaNormal.setCodigo("N001");
+		senhaNormal.setTipo(TipoSenha.NORMAL);
+		senhaNormal.setStatus(StatusSenha.AGUARDANDO);
+
+		when(senhaRepository.buscarProximaNormal())
+		     .thenReturn(Optional.of(senhaNormal));
+
+		ProximaSenhaDTO resultado = senhaService.proximaSenha();
+
+		assertEquals("N001", resultado.getCodigo());
+		assertEquals(StatusSenha.ATENDENDO, senhaNormal.getStatus());
+	}
+	
+	@Test
+	void deveChamarDuasPreferenciaisAntesDeNormal() {
+
+		when(senhaRepository.buscarSenhaEmAtendimento())
+		    .thenReturn(Optional.empty());
+
+		Senha primeira = new Senha();
+		primeira.setCodigo("P001");
+		primeira.setTipo(TipoSenha.PREFERENCIAL);
+		primeira.setStatus(StatusSenha.AGUARDANDO);
+
+		Senha segunda = new Senha();
+		segunda.setCodigo("P002");
+		segunda.setTipo(TipoSenha.PREFERENCIAL);
+		segunda.setStatus(StatusSenha.AGUARDANDO);
+
+		when(senhaRepository.buscarProximaPreferencial())
+		    .thenReturn(
+		    		Optional.of(primeira), 
+		    		Optional.of(segunda)
+		    );
+
+		ProximaSenhaDTO resultado1 = senhaService.proximaSenha();
+		ProximaSenhaDTO resultado2 = senhaService.proximaSenha();
+
+		assertEquals("P001", resultado1.getCodigo());
+		assertEquals("P002", resultado2.getCodigo());
+	}
+	
+	@Test
+	void deveChamarNormalDepoisDeDuasPreferenciais() {
+
+		when(senhaRepository.buscarSenhaEmAtendimento()).thenReturn(Optional.empty());
+
+		Senha primeira = new Senha();
+		primeira.setCodigo("P001");
+		primeira.setTipo(TipoSenha.PREFERENCIAL);
+		primeira.setStatus(StatusSenha.AGUARDANDO);
+
+		Senha segunda = new Senha();
+		segunda.setCodigo("P002");
+		segunda.setTipo(TipoSenha.PREFERENCIAL);
+		segunda.setStatus(StatusSenha.AGUARDANDO);
+
+		Senha normal = new Senha();
+		normal.setCodigo("N001");
+		normal.setTipo(TipoSenha.NORMAL);
+		normal.setStatus(StatusSenha.AGUARDANDO);
+
+		when(senhaRepository.buscarProximaPreferencial())
+		     .thenReturn(Optional.of(primeira), Optional.of(segunda));
+
+		when(senhaRepository.buscarProximaNormal())
+		     .thenReturn(Optional.of(normal));
+
+		senhaService.proximaSenha();
+		senhaService.proximaSenha();
+
+		ProximaSenhaDTO resultado3 = senhaService.proximaSenha();
+
+		assertEquals("N001", resultado3.getCodigo());
+
+	}
+	
+	@Test
+	void deveVoltarParaPreferencialQuandoNaoHouverSenhaNormal() {
+		
+		when(senhaRepository.buscarSenhaEmAtendimento())
+        .thenReturn(Optional.empty());
+		
+		Senha primeira = new Senha();
+	    primeira.setCodigo("P001");
+	    primeira.setTipo(TipoSenha.PREFERENCIAL);
+	    primeira.setStatus(StatusSenha.AGUARDANDO);
+
+	    Senha segunda = new Senha();
+	    segunda.setCodigo("P002");
+	    segunda.setTipo(TipoSenha.PREFERENCIAL);
+	    segunda.setStatus(StatusSenha.AGUARDANDO);
+
+	    Senha terceira = new Senha();
+	    terceira.setCodigo("P003");
+	    terceira.setTipo(TipoSenha.PREFERENCIAL);
+	    terceira.setStatus(StatusSenha.AGUARDANDO);
+	    
+	    when(senhaRepository.buscarProximaPreferencial())
+	         .thenReturn(
+	        		 Optional.of(primeira),
+	        		 Optional.of(segunda),
+	        		 Optional.of(terceira)
+	        		 
+	        		 );
+	    
+	    when(senhaRepository.buscarProximaNormal())
+	         .thenReturn(Optional.empty());
+	    
+	    senhaService.proximaSenha();
+	    senhaService.proximaSenha();
+	    
+	    ProximaSenhaDTO resultado =
+	    		senhaService.proximaSenha();
+	    
+	    assertEquals("P003", resultado.getCodigo());
+	    
+	    
+	}
+	
+	@Test
+	void deveLancarExcecaoQuandoNaoHaSenhasAguardando() {
+
+		when(senhaRepository.buscarSenhaEmAtendimento())
+		    .thenReturn(Optional.empty());
+
+		when(senhaRepository.buscarProximaPreferencial())
+		    .thenReturn(Optional.empty());
+
+		when(senhaRepository.buscarProximaNormal())
+		    .thenReturn(Optional.empty());
+
+		RuntimeException execao = assertThrows(
+				RuntimeException.class, 
+				() -> senhaService.proximaSenha());
+
+		assertEquals(
+				"Não há senhas aguardando.", 
+				execao.getMessage());
+	
+	}
+	
+	@Test
+	void devePreencherDataInicioAtendimentoAoChamarSenha() {
+
+		when(senhaRepository.buscarSenhaEmAtendimento())
+		    .thenReturn(Optional.empty());
+
+		Senha senhaNormal = new Senha();
+		senhaNormal.setCodigo("N001");
+		senhaNormal.setTipo(TipoSenha.NORMAL);
+		senhaNormal.setStatus(StatusSenha.AGUARDANDO);
+
+		when(senhaRepository.buscarProximaPreferencial())
+		     .thenReturn(Optional.empty());
+
+		when(senhaRepository.buscarProximaNormal())
+		     .thenReturn(Optional.of(senhaNormal));
+
+		senhaService.proximaSenha();
+
+		assertNotNull(senhaNormal.getDataInicioAtendimento());
+
+	}
+	
+	@Test
+	void deveSalvarSenhaAoChamarProximaSenha() {
+		
+		 when(senhaRepository.buscarSenhaEmAtendimento())
+             .thenReturn(Optional.empty());
+		
+		when(senhaRepository.buscarProximaPreferencial())
+        .thenReturn(Optional.empty());
+		
+		Senha senhaNormal = new Senha();
+	    senhaNormal.setCodigo("N001");
+	    senhaNormal.setTipo(TipoSenha.NORMAL);
+	    senhaNormal.setStatus(StatusSenha.AGUARDANDO);
+	    
+	    when(senhaRepository.buscarProximaNormal())
+	        .thenReturn(Optional.of(senhaNormal));
+	    
+	    senhaService.proximaSenha();
+	    
+	    verify(senhaRepository).save(senhaNormal);
+		
 	}
 	
 	
